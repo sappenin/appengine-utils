fa81888445d52a487d0ddea82c83b73f58c396c7
David Fuelling <dfuelling@Davids-MacBook-Pro-2.local>
2014-03-23 14:26:02 -0600

    Fixes #2 - AbstractObjectifyStringDao.create should not call save()
    + Also forces id setting in constructors to delegate to the setter for sub-class overriding

--- src/main/java/com/sappenin/appengine/data/dao/base/AbstractObjectifyDao.java
+++ src/main/java/com/sappenin/appengine/data/dao/base/AbstractObjectifyDao.java
@@ -1,17 +1,14 @@
 /**
  * Copyright (C) 2014 Sappenin Inc. (developers@sappenin.com)
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *         http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
  */
 package com.sappenin.appengine.data.dao.base;
 
@@ -55,7 +52,7 @@ public abstract class AbstractObjectifyDao<T extends AbstractEntity> extends Abs
 	@Idempotent
 	public void save(final T entity)
 	{
-		this.save(entity, false);
+		this.save(entity, true);
 	}
 
 	/**
--- src/main/java/com/sappenin/appengine/data/model/base/AbstractObjectifyLongEntity.java
+++ src/main/java/com/sappenin/appengine/data/model/base/AbstractObjectifyLongEntity.java
@@ -1,17 +1,14 @@
 /**
  * Copyright (C) 2014 Sappenin Inc. (developers@sappenin.com)
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *         http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
  */
 package com.sappenin.appengine.data.model.base;
 
@@ -66,7 +63,19 @@ public abstract class AbstractObjectifyLongEntity<T extends AbstractEntity> exte
 	{
 		Preconditions.checkNotNull(id);
 		Preconditions.checkArgument(id.longValue() > 0);
-		this.id = id;
+
+		// Use the setter to allow sub-classes to override this behavior
+		this.setId(id);
+	}
+
+	/**
+	 * Required-args constructor.
+	 * 
+	 * @param entityKey
+	 */
+	public AbstractObjectifyLongEntity(com.googlecode.objectify.Key<T> entityKey)
+	{
+		this(entityKey.getId());
 	}
 
 	/**
